c78ee33191bd7a8cde7704a472ebf218c1bf461c
wuliang <foxgaudi@gmail.com>
2022-12-09 00:34:42 +0800

    构建基础错误代码

---
+++ utils/error/code/user.go
@@ -0,0 +1,31 @@
+package code
+
+const (
+	/**
+	Describe 用户模块错误码
+	1000 - 1999
+	*/
+
+	UserNotExist      = 1000 // 用户不存在
+	UserPasswordWrong = 1001 // 用户密码错误
+	UserAlreadyExist  = 1002 // 用户已存在
+	UserIsDisabled    = 1003 // 用户已被禁用
+	UserIsDeleted     = 1004 // 用户已被删除
+)
+
+var userCode = map[int]string{
+	UserNotExist:      "用户不存在",
+	UserPasswordWrong: "用户密码错误",
+	UserAlreadyExist:  "用户已存在",
+	UserIsDisabled:    "用户已被禁用",
+	UserIsDeleted:     "用户已被删除",
+}
+
+func GetUserMsg(code int) string {
+	msg, ok := userCode[code]
+	if ok {
+		return msg
+	}
+
+	return userCode[UserNotExist]
+}
